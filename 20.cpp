f99bcea78a411e7975141e437a16c0026d756e2f
Frozha <116617959+Frozha@users.noreply.github.com>
2023-07-31 16:19:01 +0530

    20 complete

--- 20.cpp
+++ 20.cpp
@@ -17,81 +17,134 @@ class point{
             double dist = sqrt(pow(x-origin.x,2)+pow(y-origin.y,2)+pow(z-origin.z,2));
             return dist;
         }
+	double distance(point *origin){
+		double dist = sqrt(pow(x-origin->x,2)+pow(y-origin->y,2)+pow(z-origin->z,2));
+		return dist;
+	}
 };
 
 class wlan{
     private:
-        string uuid;
         double range;
     public:
-        point coordinate;
-        wlan(point a, string id, double r):coordinate{a},uuid{id},range{r}{}
+        point *coordinate;
+        string uuid;
+	wlan(point* a, string id, double r):coordinate{a},uuid{id},range{r}{}
         
-        bool inrange(wlan neighbor){
-            double distance = coordinate.distance(neighbor.coordinate);
-            if(distance<=range&&distance<=neighbor.range){
+        bool inrange(wlan *neighbor){
+            double distance = coordinate->distance(neighbor->coordinate);
+            if(distance<=range&&distance<=neighbor->range){
                 return true;
             }
             return false;
         }
 
         void info(){
-            cout<<endl<<uuid<<" at ("<<coordinate.x<<","<<coordinate.y<<","<<coordinate.z<<")"<<" with range : "<<range;
+            cout<<endl<<uuid<<" at ("<<coordinate->x<<","<<coordinate->y<<","<<coordinate->z<<")"<<" with range : "<<range;
         }
+	void neighbor_info(wlan *n){
+	    cout<<endl<<uuid<<" neighbor with "<<n->uuid;
+	}
+	void change_location(point *a){
+		coordinate=a;
+	}
 
 
 };
 
-vector<wlan> devices;
+/*
+class devices{
+	public: 
+	private:
+		struct inrange{
+    			int home_index;
+    			int neighbor_index;
+		};
+};
+*/
+vector<wlan*> devices;
 
 struct inrange{
-    int home_index;
-    int neighbor_index;
+	int home_index;
+	int neighbor_index;
 };
-
-vector<inrange> inrange_v;
+vector<inrange> inrange_v1,inrange_v2;
 
 int main(){
     string id = "tp-1000";
     double r;
     int x,y,z=0;
-    uniform_real_distribution<double> range_distrib(0,100);
+    uniform_real_distribution<double> range_distrib(0,25);
     uniform_real_distribution<double> xy_coordinate_distrib(-50,50);
     uniform_real_distribution<double> z_coordinate_distrib(-2,2);
     random_device rd;
     mt19937 gen(rd());
 
     for(int i=0;i<10;i++){
-        id[-1]=(char)(i+48);
+        id[6]='a'+i;
         r = range_distrib(gen);
         x = xy_coordinate_distrib(gen);
         y = xy_coordinate_distrib(gen);
         z = z_coordinate_distrib(gen);
-        point loc(x,y,z);
-        wlan dev(loc,id,r);
+	point *loc = new point(x,y,z);
+	wlan *dev = new wlan(loc,id,r);
         devices.push_back(dev);
-        delete &loc;
-        delete &dev;
-
-    }
-    for(wlan dev:devices){
-        dev.info();
     }
 
-/*
+    cout<<"initialised devices";
+    for(wlan* dev:devices){
+        dev->info();
+    }
+    
+    //cout<<"testing : \n"<<devices[0]->info()<<endl;
+
     for(int i=0;i<10;i++){
         for(int j=i+1;j<10;j++){
-            if(devices[i].inrange(devices[j])){
-                inrange_v.push_back({i,j});
+            if(devices[i]->inrange(devices[j])){
+                inrange_v1.push_back({i,j});
             }
         }
     }
 
-    for(inrange k:inrange_v){
-        cout<<endl;
-        devices.at(k.home_index).info();
-        devices.at(k.neighbor_index).info();
+    cout<<"\n\nin range devices";
+
+    for(inrange k:inrange_v1){
+        devices.at(k.home_index)->neighbor_info(devices.at(k.neighbor_index));
     }
-*/
+    
+    cout<<"\n\nnew location of devices";
+    for(wlan* dev:devices){
+	x = xy_coordinate_distrib(gen);
+        y = xy_coordinate_distrib(gen);
+        z = z_coordinate_distrib(gen);
+        point *loc1 = new point(x,y,z);
+	point *temp = dev->coordinate;
+	delete temp;
+	dev->change_location(loc1);
+    }
+    for(wlan* dev:devices){
+        dev->info();
+    }
+
+
+    for(int i=0;i<10;i++){
+        for(int j=i+1;j<10;j++){
+            if(devices[i]->inrange(devices[j])){
+                inrange_v2.push_back({i,j});
+            }
+        }
+    }
+
+    cout<<"\n\nnew range devices";
+
+    for(inrange k:inrange_v2){
+        devices.at(k.home_index)->neighbor_info(devices.at(k.neighbor_index));
+    }
+cout<<endl;	
+    for(wlan* dev:devices){
+    	delete dev->coordinate;
+	delete dev;
+    }
+
 }
 
