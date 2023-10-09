35430133f8ebb6b6e35c86654126eff70b3465e2
Frozha <116617959+Frozha@users.noreply.github.com>
2023-10-09 17:05:04 +0530

    lab 28 desg

---
+++ 28.cpp
@@ -0,0 +1,42 @@
+#include<string>
+
+struct Subject{
+	char* subject_name;
+};
+
+class Department{
+//needs to store subjects of a department
+	int dept_num;
+protected:
+	string dept_name;
+	int num_of_subjs;
+	Subject* subjs_arr;
+
+public:
+	Department(const int &dept_num, const string &dept_name, const int &num_of_subjs);
+	//will need to allocate char** on the fly in constuctor defn
+	bool has_dept_number(const int &dept_num){return this->dept_num == dept_num;}
+		 
+};
+//save objects of Department in an array
+//student gets department allocated by storing pointer to this object
+
+class Student{
+//enrolled student
+protected:
+	string name;
+	int roll_num;
+	Department* student_department;
+public:
+	Student(const string &name, const int &roll_num,const int &dept_num);
+		
+};
+class GradeCard:protected Student, private Department{
+	private:
+	Student* gradecard_student;
+	int semester_num;
+	public:
+	GradeCard(const int &roll_num,const int &semester);//will ask for subject 
+	void show()
+};
+
